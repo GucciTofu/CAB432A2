8fda8137ea75fc9b9697ba9c500a1f6eb136a347
anguyen19 <anguyen19@protonmail.com>
2021-10-20 23:56:10 +1000

    Added node + express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-test
+const express = require('express');
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+
+app.get('/',function(req,res)
+{
+    res.sendfile('index.html');
+})
+app.listen(port, function () {
+    console.log(`Express app listening at http://${hostname}:${port}/`);
+   
+});
